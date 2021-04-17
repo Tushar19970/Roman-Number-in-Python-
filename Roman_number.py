6b8e195c32e40d171dc0fa5a6c5f641e9324495d
Tushar Gupta <79837974+Tushar19970@users.noreply.github.com>
2021-04-17 16:06:32 +0530

    Add files via upload

---
+++ Roman_number.py
@@ -0,0 +1,10 @@
+
+num=int(input("Enter your number :"))
+a = ''
+roman= {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
+for i, j in roman.items():
+    if num >= i:
+        b = int(num/i)
+        num %= i
+        a += b * j
+print(a)
